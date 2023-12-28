54dfdc7d89f1f63b96f1b13939f617726e625361
miryamW <150753861+miryamW@users.noreply.github.com>
2023-12-28 18:25:24 +0200

    fixing

--- Services/TasksListService.cs
+++ Services/TasksListService.cs
@@ -6,84 +6,86 @@ using myToDoList.Interfaces;
 using System.IO;
 using Task = myToDoList.Models.Task;
 using Microsoft.AspNetCore.Hosting;
-
+using System;
 
 namespace myToDoList.Services
 {
-    public class TasksListService:ITasksListService
+    public class TasksListService : ITasksListService
     {
-       List<Task> tasksList{ get; }
-       private string fileName = "Tasks.json";
-        public TasksListService(IWebHostEnvironment webHost)
+        List<Task> tasksList { get; }
+        private string fileName = "Tasks.json";
+        public TasksListService()
         {
-                this.fileName = Path.Combine(webHost.ContentRootPath, "Data", "Tasks.json");
-                using (var jsonFile = File.OpenText(fileName))
+            this.fileName = Path.Combine(/*webHost.ContentRootPath,*/ "Data", "Task.json");
+
+            using (var jsonFile = File.OpenText(fileName))
+            {
+                tasksList = JsonSerializer.Deserialize<List<Task>>(jsonFile.ReadToEnd(),
+                new JsonSerializerOptions
                 {
-                    tasksList = JsonSerializer.Deserialize<List<Task>>(jsonFile.ReadToEnd(),
-                    new JsonSerializerOptions
-                    {
-                        PropertyNameCaseInsensitive = true
-                    });
-                }
-                // tasksList = new List<Task>
-                // {
-                //     new myToDoList.Models.Task{Id = 1, Description = "H.W", IsDone = false}
-                // };
-            
-        }
-    
-        private void saveToFile()
-        {
-            File.WriteAllText(fileName, JsonSerializer.Serialize(tasksList));
-        }
-        public List<Task> GetAll()=> tasksList;
+                    PropertyNameCaseInsensitive = true
+                });
+            }
         
-        public  Task GetById(int id)
-        {
-            return tasksList.FirstOrDefault(t=>t.Id==id);
-        }
-        public int Add(Task newTask)
-        {
-            if (tasksList.Count == 0)
-            {
-                newTask.Id = 1;
-            }
-            else
-            {
-                newTask.Id =  tasksList.Max(t=>t.Id) + 1;
-            }
-            tasksList.Add(newTask);
-            saveToFile();
-            return newTask.Id;
-        }
-        public bool Update(int id, Task newTask)
-        {
-            if (id != newTask.Id)
-                return false;
-            var existingTask = GetById(id);
-            if (existingTask == null )
-                return false;
-            var index = tasksList.IndexOf(existingTask);
-            if (index == -1 )
-                return false;
-            tasksList[index] = newTask;
-            saveToFile();
-            return true;
-        }  
-        public bool Delete(int id)
-        {
-            var existingTask = GetById(id);
-            if (existingTask == null )
-                return false;
-            var index = tasksList.IndexOf(existingTask);
-            if (index == -1 )
-                return false;
-            tasksList.RemoveAt(index);
-            saveToFile();
-            return true;
-        }  
+        // tasksList = new List<Task>
+        // {
+        //     new myToDoList.Models.Task{Id = 1, Description = "H.W", IsDone = false}
+        // };
+
     }
-    public static class TaskUtils
+
+    private void saveToFile()
+    {
+        File.WriteAllText(fileName, JsonSerializer.Serialize(tasksList));
+    }
+    public List<Task> GetAll() => tasksList;
+
+    public Task GetById(int id)
+    {
+        return tasksList.FirstOrDefault(t => t.Id == id);
+    }
+    public int Add(Task newTask)
+    {
+        if (tasksList.Count == 0)
+        {
+            newTask.Id = 1;
+        }
+        else
+        {
+            newTask.Id = tasksList.Max(t => t.Id) + 1;
+        }
+        tasksList.Add(newTask);
+        saveToFile();
+        return newTask.Id;
+    }
+    public bool Update(int id, Task newTask)
+    {
+        if (id != newTask.Id)
+            return false;
+        var existingTask = GetById(id);
+        if (existingTask == null)
+            return false;
+        var index = tasksList.IndexOf(existingTask);
+        if (index == -1)
+            return false;
+        tasksList[index] = newTask;
+        saveToFile();
+        return true;
+    }
+    public bool Delete(int id)
+    {
+        var existingTask = GetById(id);
+        if (existingTask == null)
+            return false;
+        var index = tasksList.IndexOf(existingTask);
+        if (index == -1)
+            return false;
+        tasksList.RemoveAt(index);
+        saveToFile();
+        return true;
+    }
+}
+public static class TaskUtils
 {
     public static void AddTask(this IServiceCollection services)
     {
--- Startup.cs
+++ Startup.cs
@@ -1,61 +1,61 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using myToDoList.Services;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Hosting;
+// using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.Hosting;
+// using Microsoft.Extensions.Logging;
+// using Microsoft.OpenApi.Models;
+// using Microsoft.AspNetCore.Builder;
+// using Microsoft.Extensions.DependencyInjection;
+// using myToDoList.Services;
 
-namespace myToDoList
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+// namespace myToDoList
+// {
+//     public class Startup
+//     {
+//         public Startup(IConfiguration configuration)
+//         {
+//             Configuration = configuration;
+//         }
 
-        public IConfiguration Configuration { get; }
+//         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+//         // This method gets called by the runtime. Use this method to add services to the container.
+//         public void ConfigureServices(IServiceCollection services)
+//         {
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "myToDoList", Version = "v1" });
-            });
-            services.AddTask();
-        }
+//             services.AddControllers();
+//             services.AddSwaggerGen(c =>
+//             {
+//                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "myToDoList", Version = "v1" });
+//             });
+//             services.AddTask();
+//         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "myToDoList v1"));
-            }
+//         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//         {
+//             if (env.IsDevelopment())
+//             {
+//                 app.UseDeveloperExceptionPage();
+//                 app.UseSwagger();
+//                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "myToDoList v1"));
+//             }
 
-            app.UseHttpsRedirection();
-            app.UseRouting();
-            app.UseAuthorization();
-           // app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseAuthorization();
-           // app.MapControllers();
-         //   app.Run();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
-}
+//             app.UseHttpsRedirection();
+//             app.UseRouting();
+//             app.UseAuthorization();
+//            // app.UseDefaultFiles();
+//             app.UseStaticFiles();
+//             app.UseAuthorization();
+//            // app.MapControllers();
+//          //   app.Run();
+//             app.UseEndpoints(endpoints =>
+//             {
+//                 endpoints.MapControllers();
+//             });
+//         }
+//     }
+// }
